7e4169a2f6686bc546177872cf3683f2cdaa6681
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-31 17:05:11 -0400

    code skeleton from jenkins

---
+++ code/enrich-content-REST-and-WS/routing/target/java-gen/org/dabou/types/Company.java
@@ -0,0 +1,154 @@
+
+package org.dabou.types;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+    "name",
+    "geo",
+    "active"
+})
+public class Company {
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("name")
+    private String name;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("geo")
+    private String geo;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("active")
+    private Boolean active;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @return
+     *     The name
+     */
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @param name
+     *     The name
+     */
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @return
+     *     The geo
+     */
+    @JsonProperty("geo")
+    public String getGeo() {
+        return geo;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @param geo
+     *     The geo
+     */
+    @JsonProperty("geo")
+    public void setGeo(String geo) {
+        this.geo = geo;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @return
+     *     The active
+     */
+    @JsonProperty("active")
+    public Boolean getActive() {
+        return active;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @param active
+     *     The active
+     */
+    @JsonProperty("active")
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(name).append(geo).append(active).append(additionalProperties).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Company) == false) {
+            return false;
+        }
+        Company rhs = ((Company) other);
+        return new EqualsBuilder().append(name, rhs.name).append(geo, rhs.geo).append(active, rhs.active).append(additionalProperties, rhs.additionalProperties).isEquals();
+    }
+
+}
---
+++ code/enrich-content-REST-and-WS/routing/target/java-gen/org/dabou/types/Contact.java
@@ -0,0 +1,219 @@
+
+package org.dabou.types;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+    "firstName",
+    "lastName",
+    "streetAddr",
+    "city",
+    "state",
+    "zip",
+    "phone"
+})
+public class Contact {
+
+    @JsonProperty("firstName")
+    private String firstName;
+    @JsonProperty("lastName")
+    private String lastName;
+    @JsonProperty("streetAddr")
+    private String streetAddr;
+    @JsonProperty("city")
+    private String city;
+    @JsonProperty("state")
+    private String state;
+    @JsonProperty("zip")
+    private String zip;
+    @JsonProperty("phone")
+    private String phone;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * @return
+     *     The firstName
+     */
+    @JsonProperty("firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * 
+     * @param firstName
+     *     The firstName
+     */
+    @JsonProperty("firstName")
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * 
+     * @return
+     *     The lastName
+     */
+    @JsonProperty("lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * 
+     * @param lastName
+     *     The lastName
+     */
+    @JsonProperty("lastName")
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * 
+     * @return
+     *     The streetAddr
+     */
+    @JsonProperty("streetAddr")
+    public String getStreetAddr() {
+        return streetAddr;
+    }
+
+    /**
+     * 
+     * @param streetAddr
+     *     The streetAddr
+     */
+    @JsonProperty("streetAddr")
+    public void setStreetAddr(String streetAddr) {
+        this.streetAddr = streetAddr;
+    }
+
+    /**
+     * 
+     * @return
+     *     The city
+     */
+    @JsonProperty("city")
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * 
+     * @param city
+     *     The city
+     */
+    @JsonProperty("city")
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * 
+     * @return
+     *     The state
+     */
+    @JsonProperty("state")
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * 
+     * @param state
+     *     The state
+     */
+    @JsonProperty("state")
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * 
+     * @return
+     *     The zip
+     */
+    @JsonProperty("zip")
+    public String getZip() {
+        return zip;
+    }
+
+    /**
+     * 
+     * @param zip
+     *     The zip
+     */
+    @JsonProperty("zip")
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    /**
+     * 
+     * @return
+     *     The phone
+     */
+    @JsonProperty("phone")
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * 
+     * @param phone
+     *     The phone
+     */
+    @JsonProperty("phone")
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(firstName).append(lastName).append(streetAddr).append(city).append(state).append(zip).append(phone).append(additionalProperties).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Contact) == false) {
+            return false;
+        }
+        Contact rhs = ((Contact) other);
+        return new EqualsBuilder().append(firstName, rhs.firstName).append(lastName, rhs.lastName).append(streetAddr, rhs.streetAddr).append(city, rhs.city).append(state, rhs.state).append(zip, rhs.zip).append(phone, rhs.phone).append(additionalProperties, rhs.additionalProperties).isEquals();
+    }
+
+}
